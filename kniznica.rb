c9e1f9ef776643c09ebbc4036c22373a5015e866
bublifuk <misovekarol@gmail.com>
2013-08-06 15:15:43 +0200

    pouzita dedicnost , trieda Book prebera vsetky metody od predka Library

--- kniznica.rb
+++ kniznica.rb
@@ -1,23 +1,41 @@
+require 'pry'
+
 class Library
-  @@array = Array.new
+  @@library = Library.new
   
-  attr_accessor :nazov
-  
-  
-  
-  def initialize(nazov)
-    
-    @nazov = nazov
+  def self.count_libraries 
+    @@library
     
   end
   
-
+  def initialize(nazov)
+    @nazov = nazov 
     
+  end
+  
+  
 end
+
 
 
 class Book < Library
+  @@array = Array.new
+  attr_accessor :nazov, :autor, :rok, :obsah
   
+  def self.count_books 
+    @@array.count
+  end
   
-  
+  def initialize(nazov, autor, rok, obsah)
+    @nazov = nazov
+    @autor = autor
+    @rok   = rok
+    @obsah = obsah
+    
+    
+  end
 end
+
+binding.pry
+
+# Book.new('Sedem','Janko','2000','Scifi')

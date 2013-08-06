f19e67da49730aecee144de16f42a5b45939662d
bublifuk <misovekarol@gmail.com>
2013-08-06 20:35:14 +0200

    takto by to mohlo fungovat, neviem dnes mam nejaky vypadok alebo co

--- kniznica.rb
+++ kniznica.rb
@@ -1,41 +1,38 @@
 require 'pry'
 
+
 class Library
-  @@library = Library.new
-  
-  def self.count_libraries 
-    @@library
+  def initialize
     
+    @library = []
   end
-  
-  def initialize(nazov)
-    @nazov = nazov 
+
+  def add_library(library_name)
     
+    @book = Book.new shelf_name
+
+    
+    @library << @book
   end
-  
-  
-end
 
-
-
-class Book < Library
-  @@array = Array.new
-  attr_accessor :nazov, :autor, :rok, :obsah
-  
-  def self.count_books 
-    @@array.count
-  end
-  
-  def initialize(nazov, autor, rok, obsah)
-    @nazov = nazov
-    @autor = autor
-    @rok   = rok
-    @obsah = obsah
-    
-    
+  def add_book(name, author, year, content )
+     @name = name
+     @author = author
+     @year = year
+     @content = content
   end
 end
 
+class Book
+  attr_accessor :library_name, :library
+
+  def initialize library_name
+    @library_name = library_name
+
+    
+    @book = { book_name => [] }
+  end
+end
 binding.pry
 
 # Book.new('Sedem','Janko','2000','Scifi')
